0050dc05fc38283d3a4edf2c49f9b4764c860164
Nicole Whitehead <nicole.whitehead@whiteboard-it.com>
2014-12-07 17:57:48 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+var namer = require('animal-namer');
+var express    = require('express');
+var app        = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router(); 
+
+router.get('/getName', function(req, res) {
+	namer.name().then(function (name) {
+			res.json({ name : name });	
+		}); 
+});
+
+router.get('/getAnimal', function(req, res) {
+	namer.animal().then(function (animal) {
+			res.json({ animal : animal });	
+		}); 
+});
+
+router.get('/adj/:letter', function(req, res) {
+	namer.adj(req.params.letter).then(function (adjective) {
+			res.json({ adjective : adjective });	
+		}); 
+});
+
+router.get('/', function(req, res) {
+	res.json({ message : "animal-namer API" });	
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Now using port: ' + port);
