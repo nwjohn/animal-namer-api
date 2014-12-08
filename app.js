f359f9aa937787f8b7b1846b7fdee80dab1f51f5
Duncan Smith <hello@duncanmsmith.com>
2014-12-07 23:05:59 -0600

    Whitespace

--- app.js
+++ app.js
@@ -7,31 +7,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;
-
 var router = express.Router(); 
 
 router.get('/getName', function(req, res) {
 	namer.name().then(function (name) {
-			res.json({ name : name });	
-		}); 
+		res.json({ name : name });	
+	}); 
 });
 
 router.get('/getAnimal', function(req, res) {
 	namer.animal().then(function (animal) {
-			res.json({ animal : animal });	
-		}); 
+		res.json({ animal : animal });	
+	}); 
 });
 
 router.get('/adj', function(req, res) {
 	namer.adj().then(function (adjective) {
-			res.json({ adjective : adjective });	
-		}); 
+		res.json({ adjective : adjective });	
+	}); 
 });
 
 router.get('/adj/:letter', function(req, res) {
 	namer.adj(req.params.letter).then(function (adjective) {
-			res.json({ adjective : adjective });	
-		}); 
+		res.json({ adjective : adjective });	
+	}); 
 });
 
 router.get('/', function(req, res) {
